26891d37a012e088cacd06704780c820aedeaaf9
Khushal Agarwal <khushal.agarwal987@gmail.com>
2020-10-16 19:31:11 +0530

    fix mongourl

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb://localhost:27017/test",
+    mongoURI: "mongodb+srv://fFreakUserCommunity:38283ehdnw92jw92@cluster0.q656q.mongodb.net/<dbname>?retryWrites=true&w=majority",
     SMTP_EMAIL:'hrithik.agarwal87@gmail.com',
     SMTP_PASSWORD:'lifeinnutshell4534',
     SMTP_HOST: 'smtp.gmail.com',
